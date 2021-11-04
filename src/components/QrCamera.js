cc7bade191b368aa669cc34e9cd4e1323e31a538
vatech-minsik <minsik.um@vatech.com>
2021-11-04 15:43:26 +0900

    test size of camera component

--- src/components/QrCamera.js
+++ src/components/QrCamera.js
@@ -83,8 +83,8 @@ const QrCamera = forwardRef((props, ref) => {
       if (canvas) {
         try {
           const videoRatio = video.videoWidth / video.videoHeight;
-          const width = root.clientHeight * videoRatio;
-          const height = root.clientHeight;
+          const width = root.scrollHeight * videoRatio;
+          const height = root.scrollHeight;
 
           const correctedX = width / 2 - width / 4;
           const correctedY = height / 2 - width / 4;
